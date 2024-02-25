da90af714372172aa20058ba14af25e819ef3883
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-02-25 18:42:02 +0530

    Example React - Profile Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Bio />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      src="profile-img/chamod.jpg"
+      alt="display picture"
+      className="avatar"
+    />
+  );
+}
+
+function Bio() {
+  return (
+    <div>
+      <h1>Chamod Tharuka</h1>
+      <p>
+        Dedicated and results-driven professional with 2.5+ years of experience
+        in IT project coordination within the software industry. Seeking to
+        leverage proven project management skills in a challenging role as a
+        Project Coordinator. I aim to contribute my knowledge and experience to
+        IT project teams, fostering success and simultaneously seeking
+        opportunities to enhance my career growth through valuable experiences
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML +CSS" imoji="💪" color="dodgerblue" />
+      <Skill skill="JavaScript" imoji="👍" color="orangered" />
+      <Skill skill="React" imoji="👶" color="yellow" />
+      <Skill skill="Angular" imoji="👶" color="green" />
+      <Skill skill="Git" imoji="💪" color="royalblue" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.imoji}</span>
     </div>
   );
 }
